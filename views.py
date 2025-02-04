bdc63c90851c904d72d263eaccdffbde7bf6f128
01091973QwerT <fastovskiyps4@mail.ru>
2025-02-04 21:38:55 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -3,13 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.shortcuts import render
-from django.views import View # Для классового представления
+from django.views import View
 
-# Функциональное представление
-def functional_view(request):
-    return render(request, 'second_task/function_based_view.html')
+# Функциональное представление для главной страницы
+def index_view(request):
+    return render(request, 'third_task/index.html')
 
-# Классовое представление
-class ClassBasedView(View):
+# Классовое представление для магазина
+class ShopView(View):
     def get(self, request):
-        return render(request, 'second_task/class_based_view.html')
+        shop_items = [
+            {'name': 'Игра 1', 'price': 20.99},
+            {'name': 'Игра 2', 'price': 15.50},
+            {'name': 'Игра 3', 'price': 30.00},
+        ]
+        return render(request, 'third_task/shop.html', {'shop_items': shop_items})
+
+# Классовое представление для корзины
+class CartView(View):
+    def get(self, request):
+        return render(request, 'third_task/cart.html')
