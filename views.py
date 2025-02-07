4caa7a0d4ad79200e3f4e3804dcb4f0e4d011697
01091973QwerT <fastovskiyps4@mail.ru>
2025-02-07 15:09:12 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -1,26 +1,67 @@
-from lib2to3.fixes.fix_input import context
-
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render
 from django.views import View
+from .forms import UserRegister
 
-def index_view(request):
-    return render(request, 'fourth_task/index.html', {'pagename': 'Главная страница'})
+users = ["user1", "user2", "admin"]
 
-class ShopView(View):
+class SignUpByDjango(View):
     def get(self, request):
-        return render(request, 'fourth_task/shop.html', {'games': ['Atomic Heart', 'Cyberpunk 2077']})
+        info = {}
+        form = UserRegister()
+        info['form'] = form
+        info['users'] = users
+        return render(request, 'fifth_task/registration_page.html', {'info': info})
 
-class CartView(View):
+    def post(self, request):
+        form = UserRegister(request.POST)
+        info = {'users': users}
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            repeat_password = form.cleaned_data['repeat_password']
+            age = form.cleaned_data['age']
+
+            if password != repeat_password:
+                info['error'] = 'Пароли не совпадают'
+            elif age < 18:
+                info['error'] = 'Вы должны быть старше 18'
+            elif username in users:
+                info['error'] = 'Пользователь уже существует'
+            else:
+                return render(request, 'fifth_task/registration_page.html', {'info': {'users': users, 'message': f'Приветствуем, {username}!' }}) #Изменён вывод сообщения
+        else:
+            info['error'] = 'Ошибка валидации формы'
+            info['form'] = form
+
+        return render(request, 'fifth_task/registration_page.html', {'info': info})
+
+
+class SignUpByHtml(View):
     def get(self, request):
-        return render(request, 'fourth_task/cart.html', {'pagename': 'Корзина'})
+        info = {}
+        info['users'] = users
+        return render(request, 'fifth_task/registration_page.html', {'info': info})
+
+    def post(self, request):
+        info = {'users': users}
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        repeat_password = request.POST.get('repeat_password')
+        age = request.POST.get('age')
+        try:
+            age = int(age)
+        except ValueError:
+            info['error'] = "Некорректный возраст"
+            return render(request, 'fifth_task/registration_page.html', {'info': info})
 
 
+        if password != repeat_password:
+            info['error'] = 'Пароли не совпадают'
+        elif age < 18:
+            info['error'] = 'Вы должны быть старше 18'
+        elif username in users:
+            info['error'] = 'Пользователь уже существует'
+        else:
+            return render(request, 'fifth_task/registration_page.html', {'info': {'users': users, 'message': f'Приветствуем, {username}!' }})
 
-
-
-
-
+        return render(request, 'fifth_task/registration_page.html', {'info': info})
